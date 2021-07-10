8f8e27d0bd7c6163833e8a0d8d4de04a7487c13c
Jon Snow <84160178+Sethmarks216@users.noreply.github.com>
2021-07-10 11:47:23 -0400

    game update

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var playerName = window.prompt("What is your Robots Name?");
 var playerHealth = 100;
-var playerAttack = 15;
+var playerAttack = 100;
 
 // you can also log multiple values at once like this console.log(playername, playerattack, playerHealth)
 
@@ -17,12 +17,23 @@ var fight = function() {
 enemyHealth = enemyHealth - playerAttack;
 // log a resulting message to the console so we know it worked
 console.log( playerName + "Attacked" + enemyName + " . " + enemyName + "now has " + enemyHealth + " health remaining");
+// check enemy's health
+if (enemyHealth <= 0) {window.alert(enemyName + "has DIED!");
+}
+else {window.alert(enemyName + "still has" + enemyHealth + "Health Left");
+}
+
 //subtract the value of enemyAttack from the value of playerHealth
  playerHealth = playerHealth - enemyAttack;
 //log a resulting message to the console so we know it work
 console.log(enemyName + "Attacked" + playerName + " . " + playerName + "now has" + playerHealth + "Health Remaining");
 };
-
+//check players health
+if (playerHealth <=0) { window.alert(playerName + " has died ");
+}
+else { 
+    window.alert(playerName + " Still has " + playerHealth + " Health Left ");
+}
 
 
 
